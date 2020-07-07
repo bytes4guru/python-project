bbd28cd63c60a223dc178aab1228c6033d74dfdd
Code Astronomer <byte4guru@gmail.com>
2020-07-07 15:38:37 -0600

    Add test fixtures

---
+++ src/tdd/fixtures_test.py
@@ -0,0 +1,80 @@
+import pytest
+
+@pytest.fixture()
+def setup1():
+    print("\nSetup 1!")
+
+# Argument Example
+def test1(setup1):
+    print("Executing test1!")
+    assert True
+
+# Decorator Example
+@pytest.mark.usefixtures("setup1")
+def test2():
+    print("Executing test2!")
+    assert True
+
+# autouse Example
+@pytest.fixture(autouse=True)
+def setup2():
+    print("\nSetup!")
+
+def test3():
+    print("Executing test3!")
+    assert True
+
+def test4():
+    print("Executing test4!")
+    assert True
+
+# Yield Example
+@pytest.fixture()
+def setup3():
+    print("\nSetup 3!")
+    yield
+    print("\nTeardown 3!")
+
+# Addfinalizer Example
+@pytest.fixture()
+def setup4(request):
+    print("\nSetup 4!")
+
+    def teardown_a():
+        print("\nTeardown A!")
+
+    def teardown_b():
+        print("\nTeardown B!")
+
+    request.addfinalizer(teardown_a)
+    request.addfinalizer(teardown_b)
+
+def test5(setup3):
+    print("Executing test5!")
+    assert True
+
+def test6(setup4):
+    print("Execute test6!")
+    assert True
+
+# Scope Example
+@pytest.fixture(scope="session", autouse=True)
+def setupSession():
+    print("\nSetup Session!")
+
+@pytest.fixture(scope="module", autouse=True)
+def setupModule():
+    print("\nSetup Module!")
+
+@pytest.fixture(scope="function", autouse=True)
+def setupFunction():
+    print("\nSetup Function!")
+
+class TestClass:
+    def test7(self):
+        print("Executing test7!")
+        assert True
+
+    def test8(self):
+        print("Execute test8!")
+        assert True
--- src/tdd/setup_teardown_test.py
+++ src/tdd/setup_teardown_test.py
@@ -3,73 +3,75 @@
 
 # Class Example #
 
-# Note: The setup class and teardown class methods 
-# have the @classmethod decorator applied, as they 
-# are passed in the uninstantiated class object 
-# rather than a unique instance of the class. 
+# Note: The setup class and teardown class methods
+# have the @classmethod decorator applied, as they
+# are passed in the uninstantiated class object
+# rather than a unique instance of the class.
 
 class TestClass:
-	@classmethod
-	def setup_class(cls):
-		print("Setup TestClass!")
+    @classmethod
+    def setup_class(cls):
+        print("Setup TestClass!")
 
-	@classmethod
-	def teardown_class(cls):
-		print("Teardown TestClass!")
+    @classmethod
+    def teardown_class(cls):
+        print("Teardown TestClass!")
 
-	def setup_method(self, method):
-		if method == self.test1:
-			print("\nSetting up test1!");
-		elif method == self.test2:
-			print("\nSetting up test2!");
-		else:
-			print("\nSetting up unknown test!");
-			
-	def teardown_method(self, method):
-		if method == self.test1:
-			print("\nTearing down test1!");
-		elif method == self.test2:
-			print("\nTearing down test2!");
-		else:
-			print("\nTearing down unknown test!");
-			
-	def test1(self):
-		print("Executing test1!")
-		assert True
-	
-	def test2(self):
-		print("Executing test2!")
-		assert True
+    def setup_method(self, method):
+        if method == self.test1:
+            print("\nSetting up test1!")
+        elif method == self.test2:
+            print("\nSetting up test2!")
+        else:
+            print("\nSetting up unknown test!")
+
+    def teardown_method(self, method):
+        if method == self.test1:
+            print("\nTearing down test1!")
+        elif method == self.test2:
+            print("\nTearing down test2!")
+        else:
+            print("\nTearing down unknown test!")
+
+    def test1(self):
+        print("Executing test1!")
+        assert True
+
+    def test2(self):
+        print("Executing test2!")
+        assert True
 
 
 # Module Example #
 
 def setup_module(module):
-	print("Setup Module!")
+    print("Setup Module!")
+    return module
 
 def teardown_module(module):
-	print("Teardown Module!")
+    print("Teardown Module!")
+    return module
 
 def setup_function(function):
-	if function == test1:
-		print("\nSetting up test1!");
-	elif function == test2:
-		print("\nSetting up test2!");
-	else:
-		print("\nSetting up unknown test!");
+    if function == test1:
+        print("\nSetting up test1!")
+    elif function == test2:
+        print("\nSetting up test2!")
+    else:
+        print("\nSetting up unknown test!")
 
 def teardown_function(function):
-	if function == test1:
-		print("\nTearing down test1!");
-	elif function == test2:
-		print("\nTearing down test2!");
-	else:
-		print("\nTearing down unknown test!");
+    if function == test1:
+        print("\nTearing down test1!")
+    elif function == test2:
+        print("\nTearing down test2!")
+    else:
+        print("\nTearing down unknown test!")
 
 def test1():
-	print("Executing test1!")
-	assert True
+    print("Executing test1!")
+    assert True
 
 def test2():
-	print("Executing test2!")
-	assert True
+    print("Executing test2!")
+    assert True
